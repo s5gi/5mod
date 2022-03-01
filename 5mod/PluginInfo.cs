0f604dfb279973f17f55785143120ae961a5490d
5gi <5giwastooshort@gmail.com>
2022-03-01 15:38:08 -0500

    First Commit
    + Base files

---
+++ 5mod/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace _5mod
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.the5gi.gorillatag.5mod";
+		public const string Name = "5mod";
+		public const string Version = "1.0.0";
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# 5mod
+
+Made for modding. Only works in Utilla Modded lobbies. 
+
+
+
+
+	TODO: 
+
+Create and build a working mod.
